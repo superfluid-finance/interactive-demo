0ec5b4ff898f89bc733b1915fd091bb38533ac35
Miao ZhiCheng <miao@superfluid.finance>
2021-06-08 20:36:25 +0300

    claim function and front page

---
+++ functions/claim.js
@@ -0,0 +1,13 @@
+const ethers = require("ethers");
+
+exports.handler = async function(event, context) {
+  const { ADMIN_PRIVATE_KEY, XDAI_PROVIDER_URL } = process.env;
+  const provider = new ethers.providers.JsonRpcProvider(XDAI_PROVIDER_URL);
+  //const wallet = new ethers.Wallet(ADMIN_PRIVATE_KEY);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      balance: (await provider.getBalance(ADMIN_PRIVATE_KEY)).toString(),
+    }),
+  };
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Superfluid demo</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="gifs/start_streaming.gif" className="App-logo" alt="logo" />
+        <h1>Claim a Superfluid Stream of Money on xDAI</h1>
+        <InstructionList>
+          <li>
+            Visit{" "}
+            <a
+              className="App-link"
+              href="https://app.superfluid.finance/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Superfluid App
+            </a>
+          </li>
+          <li>Select Portis -> xDAI Mainnet</li>
+          <li>Follow Portis onboarding process to register an account</li>
+          <li>
+            Your account address:{" "}
+            <AddressInput id="claim_address_input" type="text" size="42" />
+            <Button>Claim</Button>
+          </li>
+        </InstructionList>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const InstructionList = styled.ol`
+  text-align: left;
+  padding: 10px 60px;
+  margin: 0px 10px 0px !important;
+`;
+
+const AddressInput = styled.input`
+  font-size: 24px;
+  margin: 0px 10px 0px;
+`;
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 24px;
+  border-radius: 5px;
+  margin: 0px 10px 0px;
+  cursor: pointer;
+`;
