1d5860b847a1c5208693b234dbf6df1683b54abf
Miao ZhiCheng <miao@superfluid.finance>
2021-06-08 21:02:19 +0300

    add GOERLI_PROVIDER_URL

--- functions/claim.js
+++ functions/claim.js
@@ -1,7 +1,11 @@
 const ethers = require("ethers");
 
 exports.handler = async function(event, context) {
-  const { ADMIN_PRIVATE_KEY, XDAI_PROVIDER_URL } = process.env;
+  const {
+    ADMIN_PRIVATE_KEY,
+    GOERLI_PROVIDER_URL,
+    XDAI_PROVIDER_URL,
+  } = process.env;
   const body = JSON.parse(event.body);
   const provider = new ethers.providers.JsonRpcProvider(XDAI_PROVIDER_URL);
   const wallet = new ethers.Wallet(ADMIN_PRIVATE_KEY);
