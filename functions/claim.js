818e4d7854adbe5ac6089e02e07db75c01f3a282
Miao ZhiCheng <miao@superfluid.finance>
2021-06-09 14:35:57 +0300

    integrate with StreamSender

--- functions/claim.js
+++ functions/claim.js
@@ -1,19 +1,33 @@
+const ABI = require("../contracts/abi/StreamSender.abi");
 const ethers = require("ethers");
 
 exports.handler = async function(event, context) {
-  const {
-    ADMIN_PRIVATE_KEY,
-    GOERLI_PROVIDER_URL,
-    XDAI_PROVIDER_URL,
-  } = process.env;
-  const body = JSON.parse(event.body);
-  const provider = new ethers.providers.JsonRpcProvider(XDAI_PROVIDER_URL);
-  const wallet = new ethers.Wallet(ADMIN_PRIVATE_KEY);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      balance: (await provider.getBalance(body.address)).toString(),
-    }),
-  };
+  try {
+    const { ADMIN_PRIVATE_KEY, PROVIDER_URL, SS_ADDRESS } = process.env;
+    const body = JSON.parse(event.body);
+    const provider = new ethers.providers.JsonRpcProvider(PROVIDER_URL);
+    const wallet = new ethers.Wallet(ADMIN_PRIVATE_KEY);
+    const ss = new ethers.Contract(SS_ADDRESS, ABI, wallet.connect(provider));
+    const tx = await ss.claim(body.address);
+    console.log(tx);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        body: JSON.stringify({
+          status: 202,
+          tx,
+        }),
+      }),
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        status: 400,
+        error: e.toString(),
+      }),
+    };
+  }
 };
